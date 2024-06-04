ff242c0db785398b54873168de52cbb552846b7f
dkimisima@gmail.com <dkimisima@gmail.com>
2024-06-04 16:58:26 -0300

    starting the create note page

---
+++ src/components/TextArea/index.jsx
@@ -0,0 +1,14 @@
+import { Container } from "./styles";
+import PropTypes from 'prop-types';
+
+export function TextArea({ value, ...rest}) {
+    return (
+        <Container {...rest}>
+            {value}
+        </Container>
+    )
+}
+
+TextArea.propTypes = {
+    value: PropTypes.string
+}
