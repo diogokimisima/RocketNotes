bafa1e7edcfabcd96ed9fdddf09a62370ffeea88
dkimisima@gmail.com <dkimisima@gmail.com>
2024-06-05 16:53:35 -0300

    add logout

--- src/hooks/auth.jsx
+++ src/hooks/auth.jsx
@@ -1,33 +1,62 @@
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useState, useEffect } from "react";
 import PropTypes from 'prop-types';
 
-import { api } from "../services/api"
+import { api } from "../services/api";
 
 export const AuthContext = createContext({});
 
 function AuthProvider({ children }) {
-    const [ data, setData ] = useState({});
+    const [data, setData] = useState({});
 
     async function signIn({ email, password }) {
 
         try {
             const response = await api.post("/sessions", { email, password });
             const { user, token } = response.data;
-            
-           api.defaults.headers.authorization = `Bearer ${token}`;
-           setData({ user, token })
+
+            localStorage.setItem("@rocketnotes:user", JSON.stringify(user));
+            localStorage.setItem("@rocketnotes:token", token);
+
+            api.defaults.headers.authorization = `Bearer ${token}`;
+            setData({ user, token });
 
         } catch (error) {
-            if(error.response){
+            if (error.response) {
                 alert(error.response.data.message);
-            }else {
-                alert("Não foi possível entrar.")
+            } else {
+                alert("Não foi possível entrar.");
             }
         }
     }
 
+    function signOut() {
+        localStorage.removeItem("@rocketnotes:token");
+        localStorage.removeItem("@rocketnotes:user");
+
+        setData({});
+    }
+
+    useEffect(() => {
+        const token = localStorage.getItem("@rocketnotes:token");
+        const user = localStorage.getItem("@rocketnotes:user");
+
+        if (token && user) {
+            api.defaults.headers.authorization = `Bearer ${token}`;
+
+            setData({
+                token,
+                user: JSON.parse(user)
+            });
+        }
+    }, []);
+
     return (
-        <AuthContext.Provider value={{ signIn, user: data.user }}>
+        <AuthContext.Provider value={{
+            signIn,
+            signOut,
+            user: data.user,
+        }}
+        >
             {children}
         </AuthContext.Provider>
     );
@@ -41,7 +70,5 @@ function useAuth() {
 AuthProvider.propTypes = {
     children: PropTypes.node
 };
-
-
 
 export { AuthProvider, useAuth };
