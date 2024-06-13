b9dfd0ff6d7f34f0614fbb59e8028241bf3b5f3b
dkimisima@gmail.com <dkimisima@gmail.com>
2024-06-13 10:26:59 -0300

    loading the note

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,5 +1,6 @@
 import { FiPlus, FiSearch } from 'react-icons/fi';
 import { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 import { api } from '../../services/api';
 
@@ -21,6 +22,8 @@ export function Home() {
   const [tagsSelected, setTagSelected] = useState([]);
   const [notes, setNotes] = useState([]);
 
+  const navigate = useNavigate();
+
   function handleTagSelected(tagName) {
 
     if (tagName === "all") {
@@ -37,6 +40,10 @@ export function Home() {
       setTagSelected(prevState => [...prevState, tagName])
     }
 
+  }
+
+  function handleDetails(id) {
+    navigate(`/details/${id}`)
   }
 
   useEffect(() => {
@@ -102,6 +109,7 @@ export function Home() {
               <Note
                 key={String(note.id)}
                 data={note}
+                onClick={() => handleDetails(note.id)}
               />
             ))
           }
