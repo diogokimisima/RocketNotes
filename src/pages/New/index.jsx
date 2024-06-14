2a003b51fc6194ed31fb37c2f7c4db710531416a
dkimisima@gmail.com <dkimisima@gmail.com>
2024-06-14 10:37:11 -0300

    navigation adjustment

--- src/pages/New/index.jsx
+++ src/pages/New/index.jsx
@@ -1,4 +1,3 @@
-import { Link } from 'react-router-dom';
 import { useState } from 'react';
 import { toast, ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
@@ -11,6 +10,7 @@ import { NoteItem } from "../../components/NoteItem";
 import { TextArea } from "../../components/TextArea";
 import { Section } from "../../components/Section";
 import { Button } from "../../components/Button";
+import { ButtonText } from "../../components/ButtonText";
 
 import { api } from '../../services/api';
 
@@ -27,6 +27,10 @@ export function New() {
     const [newTag, setNewTag] = useState("");
 
     const navigate = useNavigate();
+
+    function handleBack() {
+        navigate(-1)
+      }
 
     function handleAddLink() {
         setLinks(prevState => [...prevState, newLink]);
@@ -69,7 +73,7 @@ export function New() {
 
         toast.success("Nota criado com sucesso");
         setTimeout(() => {
-            navigate("/");
+            navigate(-1);
         }, 2000)
 
     }
@@ -82,7 +86,7 @@ export function New() {
                 <Form>
                     <header>
                         <h1>Criar nota</h1>
-                        <Link to="/">Voltar</Link>
+                        <ButtonText onClick={handleBack} title="voltar"/>
                     </header>
 
                     <Input
